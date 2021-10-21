9377e628ff6b8a100c41b3f0339b1b5641793d7e
arteiro07 <franciscoarteiro@gmail.com>
2021-10-21 02:00:35 +0100

    Hello_ZWORLD

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const cors = require("cors");
+
+app.use(cors());
+
 app.get("/api", (req,res)=> {
   res.json({message:"Hello Zworld"});
 });
